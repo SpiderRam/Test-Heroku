b73c63169f1953ee8647408d40ece66f723b4681
Carmen Owen <doctorsout@yahoo.com>
2018-07-30 19:18:45 -0400

    Changes to simplify server for deploy test

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT ||8080;
 
 function handleRequest(request, response) {
     const path = request.url;
 
-    switch (path) {
-        case "/home":
-            displayRoot(request, response);
-            break;
-        default:
-            response.writeHead(404);
-            response.end();
-            break;
-    }
+    response.end("hello friend");
+    
+    // switch (path) {
+    //     case "/home":
+    //         displayRoot(request, response);
+    //         break;
+    //     default:
+    //         response.writeHead(404);
+    //         response.end();
+    //         break;
+    // }
 };
 
-function displayRoot(req, res) {
-    const html = `
-    <html>
-        <body>
-            <h1>Learn Heroku</h1>
-        </body>
-    </html>
-    `;
-    res.end(html);
-}
+// function displayRoot(req, res) {
+//     const html = `
+//     <html>
+//         <body>
+//             <h1>Learn Heroku</h1>
+//         </body>
+//     </html>
+//     `;
+//     res.end(html);
+// }
 
 const server = http.createServer(handleRequest);
 
